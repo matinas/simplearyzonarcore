5583e92b877b944e4884b840e492d22174257d3f
Matias Nassi <matinas88@gmail.com>
2018-08-24 05:35:19 -0300

    Cubes and AryzonHelloAR scenes and scripts

---
+++ Assets/Scripts/PlaneToggle.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using GoogleARCore.Examples.Common;
+using UnityEngine;
+
+public class PlaneToggle : MonoBehaviour {
+
+	// This script is used to toggle OFF/ON the plane visualizations once we enter Aryzon mode
+
+	public void TogglPlanes(bool toggle)
+	{
+		foreach (GameObject plane in GameObject.FindGameObjectsWithTag ("Plane"))
+		{
+            Renderer r = plane.GetComponent<Renderer> ();
+            DetectedPlaneVisualizer t = plane.GetComponent<DetectedPlaneVisualizer>();
+            r.enabled = toggle;
+            t.enabled = toggle;
+        }
+	}
+
+}
